f1e510376227fcd878d16c02f0e08c1d06acfc68
Oreo691418 <121177525+Oreo691418@users.noreply.github.com>
2025-11-30 20:25:15 +0100

    30/11/2025
    - Ajout des 3 boutons profil, offre, candidature
    reste a faire :
      - cree 3 pages html
      - redirection a ces pages par les bouton
      - ajuster le style des boutons

---
+++ WebTemplate/Etudiant/scripts.js
@@ -0,0 +1,9 @@
+
+console.log("JS OK");
+// onnées provisoires en attendant la base
+let nom = "Durant";
+let prenom = "Anne-sophie";
+
+// Remplissage dans la page
+document.getElementById("nom").textContent = nom;
+document.getElementById("prenom").textContent = prenom;
