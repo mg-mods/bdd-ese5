fbb9ea2e51196ddfd2ab89a652632c7e32c19249
Oreo691418 <121177525+Oreo691418@users.noreply.github.com>
2026-01-19 12:39:02 +0100

    Delete WebTemplate directory

--- WebTemplate/Etudiant/scripts.js
+++
@@ -1,9 +0,0 @@
-
-console.log("JS OK");
-// onnées provisoires en attendant la base
-let nom = "Durant";
-let prenom = "Anne-sophie";
-
-// Remplissage dans la page
-document.getElementById("nom").textContent = nom;
-document.getElementById("prenom").textContent = prenom;
